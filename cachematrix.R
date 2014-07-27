4d335efe9ed396d1bf973ffec98533bf22f828c8
Lobster15 <mcclint@uky.edu>
2014-07-27 10:52:02 -0400

    git commit cachematrix.R
    A
    Assign2 file
    ssign2 file
    git reset HEAD cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Generates a matrix object and caches its inverse
+## Retrieves the inverse matrix object if it has been cached
+## else generates the inverse matrix de novo
 
-## Write a short comment describing this function
-
+## Establish a matrix object and cache it 
 makeCacheMatrix <- function(x = matrix()) {
-
+		mat <- NULL
+		setmat <- function(y) {
+				x <<- y
+				mat <<- NULL
+		}
+		get <- function() x
+		setminv <- function(solve) mat <<- solve
+		getminv <- function() mat
+		list(setmat=setmat, get=get, 
+		setminv=setminv, getminv=getminv)
 }
 
 
-## Write a short comment describing this function
-
+## Retrieve cached inverse matrix or
+## if empty generate inverse matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mat <- x$getminv()
+        if (!is.null(mat)) {
+        	message("getting cached matrix")
+        	return(mat)
+        }
+        cm <- x$get()
+        mat <- solve(cm, ...)
+        x$setminv(mat)
+        mat
 }
